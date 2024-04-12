7684734fbaac763aafb093d2793a9743fdbcf7ec
satorusvessel <hellodarky8@gmail.com>
2024-04-12 17:01:14 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import { Balance } from './components/Balance'
 import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-
+import {GlobalProvider} from './context/GlobalState'
 function App() {
 
   return (
-    <>
+    <GlobalProvider>
 <Header />
 <div className="container">
 <Balance />
@@ -19,7 +19,7 @@ function App() {
 
 </div>
 
-    </>
+    </GlobalProvider>
   )
 }
 
