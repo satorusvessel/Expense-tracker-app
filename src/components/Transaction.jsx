b56eb4578f77114584138fffca490dd7c6e73a8a
satorusvessel <hellodarky8@gmail.com>
2024-04-13 09:14:35 +0300

    adds,deletes and tracks transactions

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
-export const Transaction = () => {
+export const Transaction = ({transaction}) => {
+const {deleteTransactions} = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? '-':'+';
+
   return (
-    <li className="minus">
-          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
+    <li className={transaction.amount < 0 ?'minus':'plus'}>
+          {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={()=>deleteTransactions(transaction.id)}className="delete-btn">x</button>
         </li>
   )
 }
